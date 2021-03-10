7cdc9e114f7357a29a9c809439b22fd54d0125e9
Sebas Narvaez <s.narvaez@utp.edu.co>
2021-03-09 22:02:15 -0500

    Cuarto ejercicio, sin pruebas

---
+++ src/main/java/Ejercicio4/Dice.java
@@ -0,0 +1,14 @@
+/*
+ *
+ */
+package Ejercicio4;
+
+/**
+ *
+ * @author Sebastian
+ */
+public interface Dice {
+    
+    public void roll();
+    
+}
---
+++ src/main/java/Ejercicio4/PairOfDice.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ejercicio4;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class PairOfDice implements Dice {
+    //Atributos
+    private final Random r = new Random();
+    private Integer dice1;
+    private Integer dice2;
+    
+    //Constructor
+    public PairOfDice() {
+    }
+    
+    //Métodos
+    @Override
+    public void roll(){
+        this.dice1 = r.nextInt(6)+1;
+        this.dice2 = r.nextInt(6)+1;
+    }
+    
+    public void valorLanzamiento(){
+        
+        Integer resultado = this.dice1 + this.dice2;
+        System.out.println(resultado);
+    }
+}
